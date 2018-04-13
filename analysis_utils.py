87e9402d2ad09308fc0c7d6741f6856e606d90dd
Lucas Pugens <lucaspugensf@gmail.com>
2018-04-13 11:16:45 -0300

    update

--- analysis_utils.py
+++ analysis_utils.py
@@ -2,6 +2,7 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 import os
+import csv
 import argparse
 import random
 
@@ -11,7 +12,7 @@ def argument_parser():
     parser = argparse.ArgumentParser(
         description='''Analysis over input data''')
     parser.add_argument('input_file', type=str, help='''input CSV file''')
-    parser.add_argument('--output_file', type=str, help='''output file''')
+    parser.add_argument('-o','--output_file', type=str, help='''output file''')
     parser.add_argument('-p', '--plot-correlation', action='store_true', help='plot correlation')
     parser.add_argument('--axis1', type=str, help='''first plot axis''')
     parser.add_argument('--axis2', type=str, help='''second plot axis''')
@@ -27,14 +28,13 @@ def argument_parser():
 
 
 def plot_correlation(df, args):
-    df = df.applymap(lambda x: x+((random.random()-0.5)/500)) # jitter
     plt.figure()
     points = plt.scatter(df[args.axis1],
                         df[args.axis2],
                         c=df[args.color],
                         s=3, cmap='viridis', alpha=0.7)
     plt.colorbar(points, label=args.color)
-    sns.regplot(args.axis1, args.axis2, data=df, scatter=False)
+    sns.regplot(args.axis1, args.axis2, data=df, scatter=False, x_jitter=0.05, y_jitter=0.05, order=1, robust=False)
 
     if args.output_file:
         plt.savefig(args.output_file, dpi=600)
@@ -45,7 +45,7 @@ def correlation_calculation(df, args):
     correlation = df.corr()
 
     if args.output_file:
-        correlation.to_csv(os.path.join(args.output_file, float_format='%.10f', index=True))
+        correlation.to_csv(args.output_file,  quoting=csv.QUOTE_NONNUMERIC, float_format='%.10f', index=True)
     else:
         print(correlation)
 
