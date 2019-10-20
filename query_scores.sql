e5361d3c2897e523f95bef6acfda8880d8514e5e
LPugens <lucaspugensf@gmail.com>
2019-10-20 20:42:44 -0300

    adding queries

--- query_scores.sql
+++ query_scores.sql
@@ -29,3 +29,40 @@ FROM result
          INNER JOIN score AS score_accuracy ON score_accuracy.result_id = result.id AND score_accuracy.name = 'accuracy'
          INNER JOIN arg AS arg_dataset ON arg_dataset.result_id = result.id AND arg_dataset.name = 'input_file'
          INNER JOIN arg AS arg_scenario ON arg_scenario.result_id = result.id AND arg_scenario.name = 'scenario';
+
+-- Query scores for results_1 -------------------------------------------------------------------------------------------------
+SELECT score_accuracy.value AS "ACCURACY SCORE",
+       arg_dataset.value    AS "DATASET",
+       arg_scenario.value   AS "SCENARIO"
+FROM result
+         INNER JOIN score AS score_accuracy ON score_accuracy.result_id = result.id AND score_accuracy.name = 'accuracy'
+         INNER JOIN arg AS arg_dataset ON arg_dataset.result_id = result.id AND arg_dataset.name = 'input_file'
+         INNER JOIN arg AS arg_scenario ON arg_scenario.result_id = result.id AND arg_scenario.name = 'scenario';
+
+-- Query scores for results_2 -------------------------------------------------------------------------------------------------
+SELECT score_accuracy.value     AS "ACCURACY SCORE",
+       arg_cluster.value        AS "CLUSTER METHOD",
+       arg_dataset.value        AS "DATASET",
+       arg_scenario.value       AS "SCENARIO",
+       arg_fitness_metric.value AS "FITNESS METRIC"
+FROM result
+         INNER JOIN arg AS arg_cluster ON arg_cluster.result_id = result.id AND arg_cluster.name = 'cluster_algorithm'
+         INNER JOIN score AS score_accuracy ON score_accuracy.result_id = result.id AND score_accuracy.name = 'accuracy'
+         INNER JOIN arg AS arg_dataset ON arg_dataset.result_id = result.id AND arg_dataset.name = 'input_file'
+         INNER JOIN arg AS arg_scenario ON arg_scenario.result_id = result.id AND arg_scenario.name = 'scenario'
+         INNER JOIN arg AS arg_fitness_metric
+                    ON arg_fitness_metric.result_id = result.id AND arg_fitness_metric.name = 'fitness_metric';
+
+-- Query scores for results_4_1 -------------------------------------------------------------------------------------------------
+SELECT score_accuracy.value        AS "ACCURACY SCORE",
+       arg_cluster.value           AS "CLUSTER METHOD",
+       arg_dataset.value           AS "DATASET",
+       arg_scenario.value          AS "SCENARIO",
+       COUNT(selected_features.id) AS "NUMBER OF SELECTED FEATURES"
+FROM result
+         INNER JOIN arg AS arg_cluster ON arg_cluster.result_id = result.id AND arg_cluster.name = 'cluster_algorithm'
+         INNER JOIN score AS score_accuracy ON score_accuracy.result_id = result.id AND score_accuracy.name = 'accuracy'
+         INNER JOIN arg AS arg_dataset ON arg_dataset.result_id = result.id AND arg_dataset.name = 'input_file'
+         INNER JOIN arg AS arg_scenario ON arg_scenario.result_id = result.id AND arg_scenario.name = 'scenario'
+         INNER JOIN selected_feature AS selected_features ON selected_features.result_id = result.id
+GROUP BY result.id
