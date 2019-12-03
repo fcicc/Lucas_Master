c7aac862c15d408f47a7cd68eed8af2308bf4aea
Fernando Cicconeto <fcicconeto@gmail.com>
2019-12-03 14:11:30 -0300

    eps analysis

--- plot_copac.py
+++ plot_copac.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+from numpy import arange
 from scipy.spatial import distance
 from sklearn.metrics import silhouette_score
 from package.copac import copac
@@ -10,16 +11,18 @@ X, y = parse_csv_dataset_to_df(args)
 samples_dist_matrix = distance.squareform(distance.pdist(X.values))
 results = []
 
-for k in range(2, len(X)):
-    y_pred = copac(X, k=k)
+for eps in arange(8.0, 12.0, 0.1):
+    y_pred = copac(X, k=20, mu=1, eps=eps)
     if 1 < len(set(y_pred)) < len(samples_dist_matrix):
         score = silhouette_score(samples_dist_matrix, y_pred, metric='precomputed')
     else:
         score = 0.0
-    results.append((k, score))
-    print('Computed for k=%s' % k)
+
+    results.append((eps, score))
+    print('Computed for eps=%s' % eps)
 
 plt.plot(*zip(*results))
 plt.show()
 
-# CB: k=20
+# CB: k=20, mu=1, eps=25.0
+# EM: k=123, mu=1, eps=10.0
