273de4a3796f2fc5340e34e1596605d10b1b215f
Fernando Cicconeto <fcicconeto@gmail.com>
2019-12-03 22:49:05 -0300

    EM test fix

--- plot_copac.py
+++ plot_copac.py
@@ -11,8 +11,8 @@ X, y = parse_csv_dataset_to_df(args)
 samples_dist_matrix = distance.squareform(distance.pdist(X.values))
 results = []
 
-for eps in arange(8.0, 12.0, 0.1):
-    y_pred = copac(X, k=20, mu=1, eps=eps)
+for eps in arange(1.0, 100.0, 1.0):
+    y_pred = copac(X, k=123, mu=1, eps=eps)
     if 1 < len(set(y_pred)) < len(samples_dist_matrix):
         score = silhouette_score(samples_dist_matrix, y_pred, metric='precomputed')
     else:
@@ -25,4 +25,4 @@ plt.plot(*zip(*results))
 plt.show()
 
 # CB: k=20, mu=1, eps=25.0
-# EM: k=123, mu=1, eps=10.0
+# EM: k=123, mu=1, eps=46.0
